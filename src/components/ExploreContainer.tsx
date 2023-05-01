83b7d421fb5833cab47178ed960ed8db881bbf34
benedikthalbritter <bene.halbo@gmail.com>
2023-05-01 19:53:46 +0200

    added Textarea with floating label

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,12 @@
 import './ExploreContainer.css';
+import {IonTextarea} from "@ionic/react";
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <IonTextarea label={"label"} labelPlacement={"floating"}/>
     </div>
   );
 };
